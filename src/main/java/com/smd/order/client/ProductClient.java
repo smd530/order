fdc01085cc49fcffcc03cf6eaffc23a038263444
shanmingda <shanmingda@yunshuxie.com>
2019-09-14 12:07:40 +0800

    push

---
+++ src/main/java/com/smd/order/client/ProductClient.java
@@ -0,0 +1,24 @@
+package com.smd.order.client;
+
+import com.smd.order.bean.ProductInfo;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+/**
+ * @Author: shanmingda
+ * @Date: 2019-08-12 23:48
+*/
+
+@FeignClient(name = "product")
+public interface ProductClient {
+
+    @GetMapping("/msg")
+    String productMsg();
+
+    @PostMapping("/product/listForOrder")
+    List<ProductInfo> listForOrder(@RequestBody List<String> productIdList);
+}
--- src/main/java/com/smd/order/controller/ClientController.java
+++ src/main/java/com/smd/order/controller/ClientController.java
@@ -1,40 +1,40 @@
 package com.smd.order.controller;
 
 import com.netflix.discovery.converters.Auto;
+import com.smd.order.bean.ProductInfo;
+import com.smd.order.client.ProductClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 @RestController
 @Slf4j
 public class ClientController {
 
-//    @Autowired
-//    private LoadBalancerClient loadBalancerClient;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private ProductClient productClient;
 
     @GetMapping("/getProductMsg")
     public String getProductMsg() {
-
-        //1.第一种方式(直接使用restTemplate, url写死)
-//        RestTemplate restTemplate = new RestTemplate();
-//        String response = restTemplate.getForObject("http://localhost:8080/msg", String.class);
-
-        //2.第二种方式(利用loadBanlancerClien通过应用名获取Url,然后再使用restTemplate)
-//        RestTemplate restTemplate = new RestTemplate();
-//        ServiceInstance serviceInstance = loadBalancerClient.choose("PRODUCT");
-//        String url = String.format("http://%s:%s", serviceInstance.getHost(), serviceInstance.getPort() + "/msg");
-//        String response = restTemplate.getForObject(url, String.class);
-
-        //3.第三种方式
-        String response = restTemplate.getForObject("http://PRODUCT/msg", String.class);
-        log.info(response);
+        String response = productClient.productMsg();
         return response;
+    }
+
+    @GetMapping("/getProductList")
+    public String getProductList() {
+        List<ProductInfo> list = productClient.listForOrder(Arrays.asList("164103465734242707"));
+        log.info("response {}" + list);
+
+        return "OjbK";
     }
 }
